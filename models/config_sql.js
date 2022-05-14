1b0500a3df03a922d123f78c8fbbfc959cf31c26
Leonardo Oliveira <30002511@students.ual.pt>
2022-05-14 19:58:42 +0100

    Temporary commit

--- models/config_sql.js
+++ models/config_sql.js
@@ -1,12 +1,14 @@
 const mssql = require('mssql');
 
+// https://www.npmjs.com/package/mssql
+
 exports.connect = () => {
     const config = {
-        server : 'cienciascrapper.database.windows.net,1433',
+        server : '192.168.1.120,1433',
         database : 'cienciascrapper',
-        username : 'leonardo.tome',
-        password : 'Leo3lara1901',
-        encrypt : true
+        username : 'sa',
+        password : 'qwerty',
+        encrypt : false
     }
     return new Promise((resolve, reject) => {
         mssql.connect(`Server=${config.server};Database=${config.database};User Id=${config.username};Password=${config.password};Encrypt=${config.encrypt}`)
--- server.js
+++ server.js
@@ -12,7 +12,55 @@ app.listen(PORT, () => {
 
 app.use(express.static("public_html"));
 
-const cienciaID = 'BD1A-1C89-616D';
 const test = require('./controllers/scrape_cienciavitae');
-test.doScrapeVitae(cienciaID);
+const models = require('./models/config_models');
+(async () => {
+    const cienciaID = '6F1A-06CB-E82D';
+    let result = await test.doScrapeVitae(cienciaID);
+
+    result.forEach((res) => {
+        if(res.key == 'Curriculo'){
+            models.curriculo.Crud_setRecord(res.value);
+        }
+        if(res.key == 'Curriculo_ProeficienciaIdioma'){
+            res.value.forEach((it) => {
+                models.curriculo_proeficiencia_idioma.Crud_setRecord(it);
+            })
+        }
+        if(res.key == 'Curriculo_Formacao'){
+            res.value.forEach((it) => {
+                models.curriculo_formacao.Crud_setRecord(it);
+            })
+        }
+        if(res.key == 'Curriculo_PercursoProfissional'){
+            res.value.forEach((it) => {
+                models.curriculo_percurso_profissional.Crud_setRecord(it);
+            })
+        }
+        if(res.key == 'Curriculo_Projecto'){
+            res.value.forEach((it) => {
+                models.curriculo_projecto.Crud_setRecord(it);
+            })
+        }
+        if(res.key == 'Curriculo_Producao'){
+            res.value.forEach((it) => {
+                models.curriculo_producao.Crud_setRecord(it);
+            })
+        }
+        if(res.key == 'Curriculo_Actividade'){
+            res.value.forEach((it) => {
+                models.curriculo_actividade.Crud_setRecord(it);
+            })
+        }
+
+        if(res.key == 'Curriculo_Distincao'){
+            res.value.forEach((it) => {
+                models.curriculo_distincao.Crud_setRecord(it);
+            })
+        }
+    })
+    console.log('terminado')
+
+})();
+
 
