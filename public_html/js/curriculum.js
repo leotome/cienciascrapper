eb4fbdfef16984728ae854486ddabf8942b827a2
Leonardo Oliveira <30002511@students.ual.pt>
2022-06-15 12:32:36 +0100

    Implementing retrieveHeader

--- public_html/js/curriculum.js
+++ public_html/js/curriculum.js
@@ -28,7 +28,6 @@ function retrieveCurriculum(recordId){
 }
 
 function retrieveHeader(recordId){
-    let cienciavitae_header = document.getElementById('cienciavitae_header');
     let request_url = getAPIURI() + '/curriculum/header/' + recordId;
     let request_params = {
         method : "GET"
@@ -39,7 +38,76 @@ function retrieveHeader(recordId){
         if(result.message){
             alert(result.message);
         }
-        console.log('retrieveHeader().result => ', result);
+
+        var header = result[0];
+        document.title = header.NomeCompleto + ' | ' + 'CiênciaScrapper';
+
+        let cienciavitae_header_fullname = document.getElementById('cienciavitae_header_fullname');
+        cienciavitae_header_fullname.innerHTML = '<h2>' + header.NomeCompleto + '</h2>';
+        let cienciavitae_header_resumo = document.getElementById('cienciavitae_header_resumo');
+        cienciavitae_header_resumo.innerHTML = (header.Resumo != undefined) ? '<h6>' + header.Resumo + '</h6>' : '';
+
+        let result_HTML = '';
+
+        const summaryText = 'Identificação';
+        let details_Header = `<details open><summary>${summaryText}</summary>`;
+
+        const header_TypeElement_Open = '<h5>';
+        const header_TypeElement_Close = '</h5>';
+
+        const attribute_TypeElement_Open = '<h6>';
+        const attribute_TypeElement_Close = '</h6>';
+
+        const attributeValue_TypeElement_Open = '<p>';
+        const attributeValue_TypeElement_Close = '</p>';
+        
+        let personalIdentification_Header = header_TypeElement_Open + 'IDENTIFICAÇÃO PESSOAL'    + header_TypeElement_Close;
+        let citationNames_Header          = header_TypeElement_Open + 'NOMES DE CITAÇÃO'         + header_TypeElement_Close;
+        let identifiers_Header            = header_TypeElement_Open + 'IDENTIFICADORES DE AUTOR' + header_TypeElement_Close;
+        let addresses_Header              = header_TypeElement_Open + 'MORADAS'                  + header_TypeElement_Close;
+        let emails_Header                 = header_TypeElement_Open + 'E-MAILS'                  + header_TypeElement_Close;
+        let websites_Header               = header_TypeElement_Open + 'WEBSITES'                 + header_TypeElement_Close;
+        let fields_Header                 = header_TypeElement_Open + 'DOMÍNIOS DE ATUAÇÃO'      + header_TypeElement_Close;
+
+        let personalIdentification_FullName = attribute_TypeElement_Open + 'Nome completo' + attribute_TypeElement_Close;
+
+        let identifiers_CienciaId    = attribute_TypeElement_Open + 'Ciência ID' + attribute_TypeElement_Close;
+        let identifiers_OrcidId      = attribute_TypeElement_Open + 'ORCID iD' + attribute_TypeElement_Close;
+        let identifiers_GScholarId   = attribute_TypeElement_Open + 'Google Scholar ID' + attribute_TypeElement_Close;
+        let identifiers_ResearcherId = attribute_TypeElement_Open + 'Researcher Id' + attribute_TypeElement_Close;
+        let identifiers_ScopusId     = attribute_TypeElement_Open + 'Scopus Author Id' + attribute_TypeElement_Close;
+
+        let personalIdentification_Container = '<div>' + personalIdentification_Header + 
+                                               personalIdentification_FullName + 
+                                               attributeValue_TypeElement_Open + header.NomeCompleto + attributeValue_TypeElement_Close +
+                                               '</div>';
+
+        let citationNames_Container = '<div>' + citationNames_Header + 
+                                      attributeValue_TypeElement_Open + header.NomesCitacao + attributeValue_TypeElement_Close +
+                                      '</div>';                                               
+        
+        let identifiers_Container = '<div>' + identifiers_Header +
+                                    ((header.CienciaId != undefined)       ? identifiers_CienciaId    + attributeValue_TypeElement_Open + header.CienciaId       + attributeValue_TypeElement_Close : '') + 
+                                    ((header.OrcidId != undefined)         ? identifiers_OrcidId      + attributeValue_TypeElement_Open + header.OrcidId         + attributeValue_TypeElement_Close : '') + 
+                                    ((header.GoogleScholarId != undefined) ? identifiers_GScholarId   + attributeValue_TypeElement_Open + header.GoogleScholarId + attributeValue_TypeElement_Close : '') + 
+                                    ((header.ResearcherId != undefined)    ? identifiers_ResearcherId + attributeValue_TypeElement_Open + header.ResearcherId    + attributeValue_TypeElement_Close : '') + 
+                                    ((header.ScopusAuthorId != undefined)  ? identifiers_ScopusId     + attributeValue_TypeElement_Open + header.ScopusAuthorId  + attributeValue_TypeElement_Close : '') + 
+                                    '</div>';
+
+        let addresses_Container = (header.Moradas != undefined) ? '<div>' + addresses_Header + attributeValue_TypeElement_Open + header.Moradas + attributeValue_TypeElement_Close + '</div>' : '';
+
+        let emails_Container = (header.Emails != undefined) ? '<div>' + emails_Header + attributeValue_TypeElement_Open + header.Emails + attributeValue_TypeElement_Close + '</div>' : '';
+
+        /*
+        let websites_Container = '<div>' + websites_Header +
+                                 header.Websites + 
+                                 '</div>';
+        */
+        let fields_Container = (header.DominiosAtuacao != undefined) ? '<div>' + fields_Header + attributeValue_TypeElement_Open + header.DominiosAtuacao + attributeValue_TypeElement_Close + '</div>' : '';
+        
+        result_HTML += details_Header + personalIdentification_Container + citationNames_Container + identifiers_Container + addresses_Container + emails_Container + fields_Container + '</details>';
+        let cienciavitae_header = document.getElementById('cienciavitae_header');
+        cienciavitae_header.innerHTML = '<div class="col-lg-12">' + result_HTML + '</div>';
     })
     .catch(async (error) => {
         alert('retrieveHeader().error = ' + JSON.stringify(error));
