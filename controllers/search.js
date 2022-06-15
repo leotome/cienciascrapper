e67538544ac439659cda5ae4d60226e47f245f27
Leonardo Oliveira <30002511@students.ual.pt>
2022-06-15 09:56:09 +0100

    Adding comments to custom search code

--- controllers/search.js
+++ controllers/search.js
@@ -13,9 +13,12 @@ exports.cRud_doSearchCVs = async (req, res) => {
             filters : []
         }
     })
+    // Estes filtros são construídos com base nas escolhas feitas pelos utilizadores na interface gráfica.
+    // As consultas são construídas de acordo com o tipo de dados, e no fim é criada um argumento de consulta dinâmico.
     body.forEach(item => {
         var filter = "";
         switch (item.table) {
+            // Construir consulta dinâmica para a tabela [Curriculo]
             case "Curriculo":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo].[NomeCompleto] LIKE '%" + item.value + "%' OR [Curriculo].[NomesCitacao] LIKE '%" + item.value + "%' OR [Curriculo].[Resumo] LIKE '%" + item.value + "%' OR [Curriculo].[CienciaId] LIKE '%" + item.value + "%' OR [Curriculo].[OrcidId] LIKE '%" + item.value + "%' OR [Curriculo].[GoogleScholarId] LIKE '%" + item.value + "%' OR [Curriculo].[ResearcherId] LIKE '%" + item.value + "%' OR [Curriculo].[ScopusAuthorId] LIKE '%" + item.value + "%' OR [Curriculo].[Moradas] LIKE '%" + item.value + "%' OR [Curriculo].[Emails] LIKE '%" + item.value + "%' OR [Curriculo].[DominiosAtuacao] LIKE '%" + item.value + "%'";
@@ -24,6 +27,7 @@ exports.cRud_doSearchCVs = async (req, res) => {
                     filter = "[Curriculo].[NomeCompleto] NOT LIKE '%" + item.value + "%' OR [Curriculo].[NomesCitacao] NOT LIKE '%" + item.value + "%' OR [Curriculo].[Resumo] NOT LIKE '%" + item.value + "%' OR [Curriculo].[CienciaId] NOT LIKE '%" + item.value + "%' OR [Curriculo].[OrcidId] NOT LIKE '%" + item.value + "%' OR [Curriculo].[GoogleScholarId] NOT LIKE '%" + item.value + "%' OR [Curriculo].[ResearcherId] NOT LIKE '%" + item.value + "%' OR [Curriculo].[ScopusAuthorId] NOT LIKE '%" + item.value + "%' OR [Curriculo].[Moradas] NOT LIKE '%" + item.value + "%' OR [Curriculo].[Emails] NOT LIKE '%" + item.value + "%' OR [Curriculo].[DominiosAtuacao] NOT LIKE '%" + item.value + "%'";
                 }
                 break;
+            // Construir consulta dinâmica para a tabela [Curriculo_Actividade]
             case "Curriculo_Actividade":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo_Actividade].[Tipo] LIKE '%" + item.value + "%' OR [Curriculo_Actividade].[Descricao] LIKE '%" + item.value + "%' OR [Curriculo_Actividade].[CursoInstituicao] LIKE '%" + item.value + "%'";
@@ -47,6 +51,7 @@ exports.cRud_doSearchCVs = async (req, res) => {
                     filter = "[Curriculo_Actividade].[Atual] = " + (item.value === "SIM") ? "1" : "0" + "";
                 }
                 break;
+            // Construir consulta dinâmica para a tabela [Curriculo_Distincao]
             case "Curriculo_Distincao":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo_Distincao].[Tipo] LIKE '%" + item.value + "%' OR [Curriculo_Distincao].[Descricao] LIKE '%" + item.value + "%'";
@@ -61,6 +66,7 @@ exports.cRud_doSearchCVs = async (req, res) => {
                     filter = "[Curriculo_Distincao].[Ano] <= " + item.value + "";
                 }
                 break;
+            // Construir consulta dinâmica para a tabela [Curriculo_Formacao]
             case "Curriculo_Formacao":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo_Formacao].[Descricao] LIKE '%" + item.value + "%'";
@@ -84,6 +90,7 @@ exports.cRud_doSearchCVs = async (req, res) => {
                     filter = "[Curriculo_Formacao].[Concluido] = " + (item.value === "SIM") ? "0" : "1" + "";
                 }
                 break;
+            // Construir consulta dinâmica para a tabela [Curriculo_PercursoProfissional]
             case "Curriculo_PercursoProfissional":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo_PercursoProfissional].[Tipo] LIKE '%" + item.value + "%'" + " OR " + "[Curriculo_PercursoProfissional].[CategoriaInstituicao] LIKE '%" + item.value + "%'" + " OR " + "[Curriculo_PercursoProfissional].[Empregador] LIKE '%" + item.value + "%'";
@@ -107,6 +114,7 @@ exports.cRud_doSearchCVs = async (req, res) => {
                     filter = "[Curriculo_PercursoProfissional].[Atual] = " + (item.value === "SIM") ? "1" : "0" + "";
                 }
                 break;
+            // Construir consulta dinâmica para a tabela [Curriculo_Producao]
             case "Curriculo_Producao":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo_Producao].[Tipo] LIKE '%" + item.value + "%' OR [Curriculo_Producao].[Categoria] LIKE '%" + item.value + "%' OR [Curriculo_Producao].[Descricao] LIKE '%" + item.value + "%'";
@@ -115,6 +123,7 @@ exports.cRud_doSearchCVs = async (req, res) => {
                     filter = "[Curriculo_Producao].[Tipo] NOT LIKE '%" + item.value + "%' OR [Curriculo_Producao].[Categoria] NOT LIKE '%" + item.value + "%' OR [Curriculo_Producao].[Descricao] NOT LIKE '%" + item.value + "%'";
                 }
                 break;
+            // Construir consulta dinâmica para a tabela [Curriculo_ProeficienciaIdioma]
             case "Curriculo_ProeficienciaIdioma":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo_ProeficienciaIdioma].[Idioma] LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Conversacao] LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Leitura] LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Escrita] LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Compreensao] LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[PeerReview] LIKE '%" + item.value + "%'";
@@ -123,6 +132,7 @@ exports.cRud_doSearchCVs = async (req, res) => {
                     filter = "[Curriculo_ProeficienciaIdioma].[Idioma] NOT LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Conversacao] NOT LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Leitura] NOT LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Escrita] NOT LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[Compreensao] NOT LIKE '%" + item.value + "%' OR [Curriculo_ProeficienciaIdioma].[PeerReview] NOT LIKE '%" + item.value + "%'";
                 }
                 break;
+            // Construir consulta dinâmica para a tabela [Curriculo_Projecto]
             case "Curriculo_Projecto":
                 if(item.type == "LIKE"){
                     filter = "[Curriculo_Projecto].[Tipo] LIKE '%" + item.value + "%'" + " OR " + "[Curriculo_Projecto].[Financiadores] LIKE '%" + item.value + "%'" + " OR " + "[Curriculo_Projecto].[Designacao] LIKE '%" + item.value + "%'";
@@ -160,9 +170,13 @@ exports.cRud_doSearchCVs = async (req, res) => {
         })
     })
 
+    // A consulta, do tipo "SELECT", terá as seguintes cláusulas: "LEFT JOIN", "WHERE"
     let baseQuery = "SELECT DISTINCT [Curriculo].[CienciaId], [Curriculo].[NomeCompleto] FROM [Curriculo]"
+    // Abaixo é feita a adição da cláusula LEFT JOIN
     let baseAddJoins = joins.join(" ");
+    // Abaixo é feita a adição da cláusula WHERE
     let baseAddWheres = "(" + wheres.join(") AND (") + ")";
+    // Abaixo é feita a construção dinâmica da consulta
     let fullQuery = baseQuery + " " + baseAddJoins + " WHERE " + baseAddWheres;
 
     let result = await generic.cRud_getData(fullQuery);
